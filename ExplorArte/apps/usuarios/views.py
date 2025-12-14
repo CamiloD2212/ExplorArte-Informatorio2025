04b106416a8f2d39849759a6d4ff4936d059bc24
CamiloD2212 <camilod2212@gmail.com>
2025-12-14 16:09:28 -0300

    estilos registro-redireccion de login a registro-linkedink y git de axel

--- ExplorArte/apps/usuarios/views.py
+++ ExplorArte/apps/usuarios/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
 from django.contrib import messages
+from .forms import RegistroForm   # 👈 IMPORTANTE
 
 def registrar_usuario(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegistroForm(request.POST)   # 👈 usar RegistroForm
         if form.is_valid():
             user = form.save()
 
-            # Agregar al grupo Miembro
             grupo_miembro = Group.objects.get(name="Miembro")
             user.groups.add(grupo_miembro)
 
             messages.success(request, "Registro exitoso. Ahora puedes iniciar sesión.")
             return redirect("login")
     else:
-        form = UserCreationForm()
+        form = RegistroForm()   # 👈 usar RegistroForm
 
     return render(request, "usuarios/registro.html", {"form": form})
